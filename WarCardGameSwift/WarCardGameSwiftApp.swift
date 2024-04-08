3542af8222122e0131f2d130fa52a6280f4c8808
Ethan Miller <e20098miller@gmail.com>
2024-04-08 10:55:35 -0400

    Initial Commit

---
+++ WarCardGameSwift/WarCardGameSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameSwiftApp.swift
+//  WarCardGameSwift
+//
+//  Created by Ethan Miller on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
